bef740529090d68275bc125604ceccd50e2d0885
antony waweru <anto.waweru2030@gmail.com>
2023-04-02 04:03:40 +0300

    bears organized

---
+++ lib/insert.sql
@@ -0,0 +1,10 @@
+INSERT INTO bears(name, age, sex, color, temperament, alive) 
+VALUES 
+('Mr.Chocolate', 31, 'M', 'Brown', 'Sanguine', 1),
+('Rowdy', 23, 'M', 'Black', 'Melancholic', 1),
+('Tabitha', 88, 'F', 'White', 'Choleric', 0),
+('Sergeant Brown', 19, 'M', 'Brown', 'Sanguine', 1),
+('Melissa', 72, 'F', 'Pink', 'Phlegmatic ', 1),
+('Grinch', 93, 'M', 'Green', 'Choleric ', 0),
+('Wendy', 16, 'F', 'Black', 'Melancholic', 1),
+( NULL, 23, 'M', 'White', 'Sangine', 0);
